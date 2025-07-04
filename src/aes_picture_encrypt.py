4d5f8098059f5bb673f2057342887d83b7428e6d
Bl4omArchie <grlch.alexandre@proton.me>
2025-07-05 00:07:38 +0200

    big update in aes_pic_enc script

--- src/aes_picture_encrypt.py
+++ src/aes_picture_encrypt.py
@@ -1,20 +1,56 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
+
 from PIL import Image
+from dataclasses import dataclass
+
 import numpy as np
 import os
 
 
-class AES_interface():
+@dataclass
+class Picture:
+    name: str
+    path: str
+    image: Image.Image
+    data: np.ndarray
+    data_bytes: bytes
+    state: str
+
+@dataclass
+class EncryptedPicture:
+    name: str
+    path: str
+    image: Image.Image
+    data: np.ndarray
+    data_bytes: bytes
+    state: str
+    iv: bytes
+    key: bytes
+
+
+def get_image(picture_path: str, format_file="RGBA") -> Picture:
+    if os.path.isfile(picture_path):
+        image = Image.open(picture_path).convert(format_file)
+        data = np.array(image)
+        data_bytes = data.tobytes()
+        
+        return Picture(os.path.basename(picture_path), picture_path, image, data, data_bytes, "plaintext")
+    
+    else:
+        return -1
+
+
+class AES_interface:
     @staticmethod
     def pad_image(data):
         pad_len = 16 - (len(data) % 16)
         return data + b'\0' * pad_len
 
     @staticmethod
-    def aes_encrypt(data, data_bytes, key, enc_file_path, aes_mode=AES.MODE_ECB, iv_or_nonce=None):
+    def aes_encrypt(data, data_bytes, key, enc_file_path, iv_or_nonce=None, aes_mode=AES.MODE_ECB):
         if aes_mode in [AES.MODE_ECB, AES.MODE_CBC, AES.MODE_CFB, AES.MODE_OFB]:
-            cipher = AES.new(key, aes_mode, iv_or_nonce) if iv_or_nonce else AES.new(key, aes_mode)
+            cipher = AES.new(key, aes_mode, iv=iv_or_nonce) if iv_or_nonce else AES.new(key, aes_mode)
         elif aes_mode == AES.MODE_CTR:
             cipher = AES.new(key, aes_mode, nonce=iv_or_nonce)
 
@@ -29,6 +65,7 @@ class AES_interface():
 
         encrypted_image = Image.fromarray(encrypted_array)
         encrypted_image.save(enc_file_path)
+        
         return encrypted_image
 
     @staticmethod
@@ -36,7 +73,7 @@ class AES_interface():
         data_bytes = enc_img_array.tobytes()
 
         if aes_mode in [AES.MODE_ECB, AES.MODE_CBC, AES.MODE_CFB, AES.MODE_OFB]:
-            cipher = AES.new(key, aes_mode, iv_or_nonce) if iv_or_nonce else AES.new(key, aes_mode)
+            cipher = AES.new(key, aes_mode, iv=iv_or_nonce) if iv_or_nonce else AES.new(key, aes_mode)
         elif aes_mode == AES.MODE_CTR:
             cipher = AES.new(key, aes_mode, nonce=iv_or_nonce)
 
@@ -47,92 +84,103 @@ class AES_interface():
 
         decrypted_image = Image.fromarray(decrypted_array)
         decrypted_image.save(dec_file_path)
+        
         return decrypted_image
 
 
 class Picture_Encryption(AES_interface):
-    def __init__(self, format_file="RGBA", storage_folder="pic/", aes_mode=AES.MODE_ECB):
+    def __init__(self, pictures: list[Picture], format_file="RGBA", storage_folder="pic/"):
         self.format_file = format_file
         self.storage_folder = storage_folder
-        self.aes_mode = aes_mode
-        self.set_images = {}
-        self.enc_map = {}
+        self.pictures = pictures
+        self.enc_pictures = []
+        os.makedirs(self.storage_folder, exist_ok=True)
 
-        self.enc_folder = os.path.join(storage_folder, "encrypted")
-        self.dec_folder = os.path.join(storage_folder, "decrypted")
-
-        os.makedirs(self.enc_folder, exist_ok=True)
-        os.makedirs(self.dec_folder, exist_ok=True)
-
-    def upload_image(self, picture_path):
-        if os.path.isfile(picture_path):
-            image = Image.open(picture_path).convert(self.format_file)
-            data = np.array(image)
-            data_bytes = data.tobytes()
-            self.set_images[picture_path] = [image, data, data_bytes, "plaintext"]
-        else:
-            return -1
-
-    def upload_folder(self, folder):
+    @classmethod
+    def upload_folder(cls, folder: str, format_file="RGBA", storage_folder="pic/"):
+        pics = []
         if os.path.exists(folder):
             for filename in os.listdir(folder):
                 full_path = os.path.join(folder, filename)
-                self.upload_image(full_path)
+                img = get_image(full_path, format_file)
+                if img != -1:
+                    pics.append(img)
+        
+        return cls(pics, format_file, storage_folder)
 
-    def encrypt_picture(self):
-        if not self.set_images:
+    def encrypt_pictures(self, aes_mode=AES.MODE_ECB) -> None:
+        if not self.pictures:
             raise ValueError("Please upload pictures")
 
-        for i, (filename, data_image) in enumerate(list(self.set_images.items())):
+        for pic in self.pictures:
             key = get_random_bytes(16)
 
-            if self.aes_mode in [AES.MODE_CBC, AES.MODE_CFB, AES.MODE_OFB]:
-                iv = get_random_bytes(16)
-                iv_or_nonce = iv
-            elif self.aes_mode == AES.MODE_CTR:
+            if aes_mode in [AES.MODE_CBC, AES.MODE_CFB, AES.MODE_OFB]:
+                iv_or_nonce = get_random_bytes(16)
+            elif aes_mode == AES.MODE_CTR:
                 iv_or_nonce = get_random_bytes(8)
             else:
                 iv_or_nonce = None
 
-            enc_name = f"encrypted_{i}.png"
-            enc_path = os.path.join(self.enc_folder, enc_name)
+            enc_name = f"{pic.name}_encrypted.png"
+            enc_path = os.path.join(self.storage_folder, enc_name)
 
             encrypted_img = self.aes_encrypt(
-                data_image[1], data_image[2], key, enc_path, self.aes_mode, iv_or_nonce
+                data=pic.data,
+                data_bytes=pic.data_bytes,
+                key=key,
+                enc_file_path=enc_path,
+                iv_or_nonce=iv_or_nonce,
+                aes_mode=aes_mode
             )
 
-            self.set_images[enc_path] = [encrypted_img, np.array(encrypted_img), np.array(encrypted_img).tobytes(), "enc"]
-            self.enc_map[enc_path] = (filename, key, iv_or_nonce)
+            self.enc_pictures.append(EncryptedPicture(
+                name=enc_name,
+                path=enc_path,
+                image=encrypted_img,
+                data=np.array(encrypted_img),
+                data_bytes=np.array(encrypted_img).tobytes(),
+                state="encrypted",
+                iv=iv_or_nonce,
+                key=key
+            ))
 
-    def decrypt_folder(self):
-        for i, (enc_filename, data_image) in enumerate({k: v for k, v in self.set_images.items() if v[3] == "enc"}.items()):
-            original_filename, key, iv_or_nonce = self.enc_map.get(enc_filename, (None, None, None))
-            if key is None:
-                continue
-
-            dec_name = f"decrypted_{i}.png"
-            dec_path = os.path.join(self.dec_folder, dec_name)
+    def decrypt_pictures(self, aes_mode=AES.MODE_ECB) -> None:
+        for pic in self.enc_pictures:
+            dec_name = f"{pic.name}_decrypted.png"
+            dec_path = os.path.join(self.storage_folder, dec_name)
 
             decrypted_img = self.aes_decrypt(
-                data_image[1], key, dec_path, self.aes_mode, iv_or_nonce
+                enc_img_array=pic.data,
+                key=pic.key,
+                dec_file_path=dec_path,
+                aes_mode=aes_mode,
+                iv_or_nonce=pic.iv
             )
 
-            self.set_images[dec_path] = [decrypted_img, np.array(decrypted_img), np.array(decrypted_img).tobytes(), "dec"]
+            self.pictures.append(Picture(
+                name=dec_name,
+                path=dec_path,
+                image=decrypted_img,
+                data=np.array(decrypted_img),
+                data_bytes=np.array(decrypted_img).tobytes(),
+                state="decrypted"
+            ))
 
-    def get_state(self):
+    def get_state(self) -> None:
         print("========================")
-        for key, val in self.set_images.items():
-            print(f"{key} : {val[0].size} - {val[-1]}")
+        for pic in self.pictures:
+            print(f"{pic.name} : {pic.image.size} - {pic.state}")
+
 
 
 if __name__ == "__main__":
     # AES.MODE_CBC ou AES.MODE_ECB, AES.MODE_CTR, etc.
     mode = AES.MODE_ECB
 
-    obj = Picture_Encryption(aes_mode=mode)
-    obj.upload_folder("pic/originals/")
+    obj = Picture_Encryption.upload_folder("/your/path/")
     obj.get_state()
 
-    obj.encrypt_picture()
-    obj.decrypt_folder()
+    obj.encrypt_pictures(aes_mode=AES.MODE_ECB)
+    obj.decrypt_pictures()
     obj.get_state()
