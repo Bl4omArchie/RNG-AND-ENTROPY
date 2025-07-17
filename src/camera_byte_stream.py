fcdeef5c07dea17bf022aee5c5b5f5f3cb467307
Bl4omArchie <grlch.alexandre@proton.me>
2025-07-17 03:23:53 +0200

    update camera script

--- src/camera_byte_stream.py
+++ src/camera_byte_stream.py
@@ -1,6 +1,4 @@
-from PIL import Image
 import cv2
-
 
 def stream_camera():
     cap = cv2.VideoCapture(0)
@@ -9,45 +7,24 @@ def stream_camera():
         print("Error: Could not open camera.")
         return
 
-    while cv2.waitKey(1) != ord('q'):
-        ret, frame = cap.read()
-        if not ret:
-            print("Error: Cannot receive frame (stream end?). Exiting...")
-            break
+    try:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print("Error: Failed to grab frame.")
+                break
 
-        frame_bytes = frame.tobytes()
-        print (frame_bytes)
+            cv2.imshow('Camera Stream', frame)
 
+            frame_bytes = frame.tobytes()
+            print(frame_bytes)
 
-    cap.release() 
-    cv2.destroyAllWindows()
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-
-def test():
-    cap = cv2.VideoCapture(0)
-    scaling_factor = 1
-
-    while True:
-        ret, frame = cap.read()
-        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
-        cv2.imwrite('capture.jpg', frame)
-
-        input_image = Image.open("capture.jpg")
-        width, height = input_image.size
-        
-        for i in range(2):
-            for j in range(2):
-                print (input_image.getpixel((i, j)))
-                print ("\n")
-
-        print (rgb[0][0])
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     stream_camera()
